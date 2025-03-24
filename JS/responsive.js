94aef58968becad45685a26a3f4ec6fb0e7f8b8c
YostinaAziz <y.a.aziz@student.aast.edu>
2025-03-24 22:51:02 +0200

    Add files via upload

---
+++ JS/responsive.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+        var menu = document.getElementById("mobileMenu");
+        if (menu.style.display === "block") {
+            menu.style.display = "none";
+        } else {
+            menu.style.display = "block";
+        } 
+}
